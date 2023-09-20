bf4242f65e918298c72720ac1843c7f9c68bedc7
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-09-20 10:42:27 +0800

    Add pages

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('tags/', views.tags, name='tags'),
+    path('authors/', views.authors, name='authors'),
+    path('series/', views.series, name='series'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<b>Hello World. You are in the blog index<b>')
+    return render(request,'blog/index.html')
+
+def tags(request):
+    return render(request,'blog/tags.html')
+
+def authors(request):
+    return render(request,'blog/authors.html')
+
+def series(request):
+    return render(request,'blog/series.html')
 
 # Create your views here.
